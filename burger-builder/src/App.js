764f772778bb8fdcf2a5bfb842ef1bebf3c9cdf4
Bhaavani <vani.uideveloper@gmail.com>
2020-01-04 21:37:57 -0500

    Creating a Layout Components

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
 
 class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Layout>Test</Layout>
+      </div>
+    );
   }
 }
 
