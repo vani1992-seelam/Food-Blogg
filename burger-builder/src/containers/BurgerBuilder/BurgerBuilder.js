779f2101d2f4ff4ed7ece714bcec8c966596bbb5
Bhaavani <vani.uideveloper@gmail.com>
2020-01-06 20:37:37 -0500

    connecting state to build control and adding methods

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  meat: 1.5,
+  bacon: 0.5,
+  cheease: 1.0
+};
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -9,13 +16,27 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0,
       bacon: 0
-    }
+    },
+    totalprice: 4
   };
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalprice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({ totalprice: newPrice, ingredients: updatedIngredients });
+  };
+  removeIngredientHandler = type => {};
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </Aux>
     );
   }
