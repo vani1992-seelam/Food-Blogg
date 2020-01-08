9622b16995818478a5a82cb950c1968c890add84
Bhaavani <vani.uideveloper@gmail.com>
2020-01-08 18:42:40 -0500

    order backdrop component

--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,16 +1,21 @@
 import React from "react";
 
 import classes from "./Modal.css";
+import Aux from "../../../hoc/Aux";
+import Backdrop from "../Backdrop/Backdrop";
 const modal = props => (
-  <main
-    className={classes.Modal}
-    style={{
-      transform: props.show ? "translate(0)" : "translateY(-100vh)",
-      opacity: props.show ? "1" : "0"
-    }}
-  >
-    {props.children}
-  </main>
+  <Aux>
+    <Backdrop show={props.show} clicked={props.modalClosed} />
+    <div
+      className={classes.Modal}
+      style={{
+        transform: props.show ? "translate(0)" : "translateY(-100vh)",
+        opacity: props.show ? "1" : "0"
+      }}
+    >
+      {props.children}
+    </div>
+  </Aux>
 );
 
 export default modal;
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -64,9 +64,12 @@ class BurgerBuilder extends Component {
     this.updatePurchaseState(updatedIngredients);
   };
 
-  purchaseHandler() {
+  purchaseHandler = () => {
     this.setState({ purchasing: true });
-  }
+  };
+  purchaseCancelHandler = () => {
+    this.setState({ purchasing: false });
+  };
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -76,7 +79,10 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
