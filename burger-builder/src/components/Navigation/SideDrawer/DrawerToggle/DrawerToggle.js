06ad4aca0539d8facad844aa3644767cc7fcb88a
Bhaavani <vani.uideveloper@gmail.com>
2020-01-13 22:15:12 -0500

    fixing the DrawerToggle button

--- burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./DrawerToggle.css";
 
 const drawerToggle = props => (
-  <div onClick={props.clicked} className={classes.DrewerToggle}>
+  <div onClick={props.clicked} className={classes.DrawerToggle}>
     <div></div>
     <div></div>
     <div></div>
