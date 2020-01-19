c02657f7932da5f3c49f4a8c27558db94adc5450
Bhaavani <vani.uideveloper@gmail.com>
2020-01-18 20:50:48 -0500

    Setting default global configurations for axios

--- http_ajax/src/index.js
+++ http_ajax/src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import axios from "axios";
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
 axios.interceptors.request.use(
   request => {
