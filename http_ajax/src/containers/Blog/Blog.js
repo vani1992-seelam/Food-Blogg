8b5022617d3b5bc027f43f8b83eb53908b163314
Bhaavani <vani.uideveloper@gmail.com>
2020-01-18 20:21:43 -0500

    Handling errors by giving ui response or console error message

--- http_ajax/src/containers/Blog/Blog.js
+++ http_ajax/src/containers/Blog/Blog.js
@@ -9,38 +9,49 @@ import "./Blog.css";
 class Blog extends Component {
   state = {
     posts: [],
-    selectedPostId: null
+    selectedPostId: null,
+    error: false
   };
   componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
-      const posts = response.data.slice(0, 4);
-      const updatedPosts = posts.map(post => {
-        return {
-          ...post,
-          author: "BHAAVANI"
-        };
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "BHAAVANI"
+          };
+        });
+        this.setState({ posts: updatedPosts });
+        //console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({ error: true });
       });
-      this.setState({ posts: updatedPosts });
-      //console.log(response);
-    });
   }
 
   postSelectedHandler = id => {
     this.setState({ selectedPostId: id });
   };
   render() {
-    const posts = this.state.posts.map(post => {
-      return (
-        <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => {
-            this.postSelectedHandler(post.id);
-          }}
-        />
-      );
-    });
+    let posts = <p style={{ textAlign: "center" }}>Something went wrong!!!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return (
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => {
+              this.postSelectedHandler(post.id);
+            }}
+          />
+        );
+      });
+    }
+
     return (
       <div>
         <section className="Posts">{posts}</section>
